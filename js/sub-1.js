b1af4914a450d44c50f5af2b166efae15e21a09d
MLsanta <MLsanta527@gmail.com>
2025-10-17 15:25:34 +0900

    js 서브페이지 섹션 1 완성

--- js/sub-1.js
+++ js/sub-1.js
@@ -7,14 +7,29 @@ const img__container2 = document.querySelector(".img__container2");
 const img__container3 = document.querySelector(".img__container3");
 const img__container4 = document.querySelector(".img__container4");
 const desc__sub__container1 = document.querySelector(".desc__sub__container1");
+const desc__sub__container2 = document.querySelector(".desc__sub__container2");
+const desc__sub__container3 = document.querySelector(".desc__sub__container3");
+const desc__sub__container4 = document.querySelector(".desc__sub__container4");
 const desc__main__contents1 = document.querySelector(".desc__main__contents1");
+const desc__main__contents2 = document.querySelector(
+    ".desc__main__contents2 h1"
+);
+const desc__main__contents3 = document.querySelector(
+    ".desc__main__contents3 h1"
+);
+const desc__main__contents4 = document.querySelector(
+    ".desc__main__contents4 h1"
+);
 
 const s__top__1 = sec1.offsetTop;
+const s__top__2 = sec2.offsetTop;
+const s__top__3 = sec3.offsetTop;
+const s__top__4 = sec4.offsetTop;
 
 window.addEventListener("scroll", () => {
     let sy = window.scrollY;
 
-    if (sy >= s__top__1 - 400) {
+    if (sy >= s__top__1 - 400 && sy < s__top__1) {
         desc__main__contents1.classList.add("desc__main__contents1__active");
         img__container1.classList.add("img__container__active");
         desc__sub__container1.classList.add("desc__sub__container__active");
@@ -22,5 +37,44 @@ window.addEventListener("scroll", () => {
         desc__main__contents1.classList.remove("desc__main__contents1__active");
         img__container1.classList.remove("img__container__active");
         desc__sub__container1.classList.remove("desc__sub__container__active");
+    }
+    if (sy >= s__top__2 - 500 && sy < s__top__2) {
+        document
+            .querySelector(".desc__main__contents2 h1")
+            .classList.add("desc__main__contents__active");
+        img__container2.classList.add("img__container__active");
+        desc__sub__container2.classList.add("desc__sub__container__active");
+    } else {
+        document
+            .querySelector(".desc__main__contents2 h1")
+            .classList.remove("desc__main__contents__active");
+        img__container2.classList.remove("img__container__active");
+        desc__sub__container2.classList.remove("desc__sub__container__active");
+    }
+    if (sy >= s__top__3 - 500 && sy < s__top__3) {
+        document
+            .querySelector(".desc__main__contents3 h1")
+            .classList.add("desc__main__contents__active");
+        img__container3.classList.add("img__container__active");
+        desc__sub__container3.classList.add("desc__sub__container__active");
+    } else {
+        document
+            .querySelector(".desc__main__contents3 h1")
+            .classList.remove("desc__main__contents__active");
+        img__container3.classList.remove("img__container__active");
+        desc__sub__container3.classList.remove("desc__sub__container__active");
+    }
+    if (sy >= s__top__4 - 500 && sy < s__top__4) {
+        document
+            .querySelector(".desc__main__contents4 h1")
+            .classList.add("desc__main__contents__active");
+        img__container4.classList.add("img__container__active");
+        desc__sub__container4.classList.add("desc__sub__container__active");
+    } else {
+        document
+            .querySelector(".desc__main__contents4 h1")
+            .classList.remove("desc__main__contents__active");
+        img__container4.classList.remove("img__container__active");
+        desc__sub__container4.classList.remove("desc__sub__container__active");
     }
 });
