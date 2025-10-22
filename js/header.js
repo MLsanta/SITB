ac299ed58f27b0139f985ee795f7b6fb0b5ebaea
“황현규” <hyeongyu404@gmail.com>
2025-10-22 17:40:16 +0900

    sub-4 작업중

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ const header = document.querySelector(".hd");
 window.addEventListener("scroll", () => {
     let sy = window.scrollY;
 
-    if (sy > 30) {
+    if (sy > 20) {
         header.classList.add("header__active");
     } else {
         header.classList.remove("header__active");
