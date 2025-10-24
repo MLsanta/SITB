4ab15359b945466b60f28cf05bbe54ed59b1d632
소정1111 <banahs7029kq@naver.com>
2025-10-24 17:31:37 +0900

    섹션4 자바 수정

--- js/sec-4.js
+++ js/sec-4.js
@@ -9,7 +9,6 @@ function revealSection4() {
   const descTop = desc.getBoundingClientRect().top;
   const newsTop = news.getBoundingClientRect().top;
 
-  // 각 요소별로 화면에 들어오면 active 클래스 추가
   if (titleTop < winH * 0.9) title.classList.add("active");
   if (descTop < winH * 0.9) desc.classList.add("active");
   if (newsTop < winH * 0.9) news.classList.add("active");
